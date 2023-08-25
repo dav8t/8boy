ab0c21ec18536b27c0f6877e9dbf0d927136f761
0xgobinath <94050592+0xgobinath@users.noreply.github.com>
2023-08-25 16:10:13 +0530

    added function for 'rom'

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"flag"
+	"github.com/faiface/mainthread"
+	"github.com/sqweek/dialog"
+	"runtime/pprof"
+)
+
 var (
-	cpuprofile = flag.string("cpuprofile", "", "write cpu profile to file(debugging)")
+	mute       = flag.Bool("mute", false, "mute sound output")
+	dmgMode    = flag.Bool("dmg", false, "set to force dmg mode")
+	unlocked   = flag.Bool("unlocked", false, "if to unlock the cpu speed(debugging)")
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file(debugging)")
 )
 
 func start() {
@@ -15,4 +25,19 @@ func start() {
 	if *unlocked {
 		*mute = mute
 	}
+
+	fmt.println(fmt.Sprintf(logo, version))
+	fmt.Printf("APU: %v\nCGB: %v\nROM: %v\n", !*mute, !*dmgMode, rom)
+}
+
+func getROM() string {
+	rom := flag.Arg(0)
+	if rom == "" {
+		mainthread.Call(func() {
+			var err error
+			rom, err = dialog.File().
+				Filter("GameBoy ROM File").Load()
+		})
+	}
+	return rom
 }
